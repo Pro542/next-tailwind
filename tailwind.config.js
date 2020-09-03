17bd0b9c1a98745d794236c665c3026ae1c7421d
Praveen Senadheera <prasenadheera@gmail.com>
2020-09-03 20:36:28 +0400

    Add first-of-type variant plugin for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,21 @@
+const plugin = require('tailwindcss/plugin');
+
+function firstOfTypeVariant({ addVariant, e }) {
+    addVariant('firstOfType', ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+            return `.${e(`firstOfType${separator}${className}`)}:first-of-type`;
+        })
+    })
+}
+
 module.exports = {
     future: {
         removeDeprecatedGapUtilities: true,
-    }
+    },
+    plugins: [
+        plugin(firstOfTypeVariant)
+    ],
+    variants: {
+        margin: ['responsive', 'firstOfType'],
+    },
 }
